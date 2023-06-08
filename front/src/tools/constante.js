1331bb46b73370f6a372ba14285836455fff85be
Ayoub BAYED <ayoub@code42.fr>
2023-06-08 10:38:33 +0200

    [#?] Fixed : Url redirect on front

--- front/src/tools/constante.js
+++ front/src/tools/constante.js
@@ -2,7 +2,7 @@
 // export const BASE_URL = "http://localhost:3001";
 // export const BASE_IMG = "http://localhost:3001/img";
 
-export const BASE_URL = "http://www.leshommessensibles.fr/";
+export const BASE_URL = "http://www.leshommessensibles.fr";
 export const BASE_IMG = "http://www.leshommessensibles.fr/img";
 
 //export const BASE_URL = "http://www.leshommessensibles.fr:3001/";
